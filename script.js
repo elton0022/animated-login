bd1a58191e27a639a8604cb089ced71a974cf487
elton0022 <eltonrodrigues0022.er@gmail.com>
2020-12-31 17:44:38 -0300

    update

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ function login(){
     let email = $('#email').val();
     let password = $('#password').val();
 
+    clearErrors();
+
     if(email == ''){
-        $("#email").effect("shake","slow");
+        $('#email').effect('shake','slow');
+        $('#error-email').text("O email não pode estar vazio!!");
     }else if(password == ''){
-        $("#password").effect("shake","slow");
-    }else if(email == 'maria@email.com' && password == '1234'){
+        $('#password').effect('shake','slow');
+        $('#error-password').text("A senha não pode estar vazia!!");
+    }else if(email == 'email@email.com' && password == '1234'){
         alert('login bem sucedido!!')
     }else{
-        $(".card").effect("shake", "slow");
+        $('.card').effect('shake','slow');
+        $('#error-login').text("Email ou senha Incorreta!!");
     }   
+}
+
+
+function clearErrors(){
+    $('#error-email').text('');
+    $('#error-password').text('');
+    $('#error-login').text('');
 }
